a5694e0ebf08a1e2be333f306c3d405ab59266eb
MOHAMED AFSAL J <99009790+afsalJ@users.noreply.github.com>
2024-04-04 12:47:26 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,16 +1,20 @@
 import java.util.*;
-public class Bank {
-    String bankName;
-    long amount;
-    HashMap<String, Customer> customers = new HashMap<>();
 
-    Bank(String bankName, long amount){
+public class Bank {
+    String bankName; //name of the bank
+    long amount; // balance amount in the bank
+    HashMap<String, Customer> customers = new HashMap<>();//customer details of the bank with account number as their key
+
+    Bank(String bankName, long amount){ //creates a bank with a name and an initial amount
         this.bankName=bankName;
         this.amount=amount;
         System.out.println("Bank craeted!");
     }
-
-    void addCustomer(String ac_number, String name, long amount){
+    void delete(){ // deletes the existing customer data
+        customers=new HashMap<>();
+        System.out.println("Account deleted Successfully");
+    }
+    void addCustomer(String ac_number, String name, long amount){ // used to add a customer with (0 balance) or (with some initial balance)
         if(customers.containsKey(ac_number)){
             System.out.println("Customer already exist!");
         }
@@ -24,8 +28,17 @@ public class Bank {
             System.out.println("Customer created!");
         }
     }
-
-    boolean validate(String ac_number, int pin){
+    void deleteCustomer(String ac_number){ // deletes the customer data(object) from the map
+        if(customers.containsKey(ac_number)){
+            customers.remove(ac_number);
+            System.out.println("Customer deleted!");
+        }
+        else{
+            System.out.println("Customer doesn't exist!");
+        }
+    }
+    
+    boolean validate(String ac_number, int pin){ // checks whether a customer with the given account number!
         if(customers.containsKey(ac_number)){
             return customers.get(ac_number).validate(pin);
         }   
@@ -34,3 +47,4 @@ public class Bank {
         }  
     }
 }
+        
