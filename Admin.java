eb207fe3a70add8eab6243bda17569b661de901e
Subash AL <129574007+Subash3002@users.noreply.github.com>
2024-04-04 12:03:28 +0530

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Admin {
+    
+    HashMap<String,Bank> banks=new HashMap<>();
+    
+    void createBank(String BankName,Long amount){
+        if(!banks.containsKey(BankName)){
+            banks.put(BankName,new Bank(BankName,amount));
+        }
+        
+    }
+
+    HashMap<String,Bank> getBanks(){
+        return banks;
+    }
+
+    void addCustomer(String AccountNumber,String HoldersName,Long amt,String BankName){
+        banks.get(BankName).addCustomer(AccountNumber,HoldersName,amt);
+    }
+
+  
+}
